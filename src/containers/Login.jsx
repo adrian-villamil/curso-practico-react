7fc9a5de0a46a22714a61d38cac000e98f96c91b
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-02-01 23:05:32 -0500

    Se agregó alias a los imports y se importó los íconos y/o logos para cada etiqueta img. Además se modificó algunos className.

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import '../styles/Login.scss';
+import '@styles/Login.scss';
+import logo_yard_sale from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
   return (
-    <div className="login">
-      <div className="form-container">
-        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+    <div className="login-container">
+      <div className="login-form-container">
+        <img src={logo_yard_sale} alt="logo" className="logo" />
         <h1 className="title">Create a new password</h1>
         <p className="subtitle">Enter a new passwrd for yue account</p>
-        <form action="/" className="form">
+        <form action="/" className="login-form">
           <label htmlFor="password" className="label">Password</label>
           <input type="password" id="password" placeholder="*********" className="input input-password" />
           <label htmlFor="new-password" className="label">Password</label>
--- src/containers/RecoveryPassword.jsx
+++ src/containers/RecoveryPassword.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import '../styles/RecoveryPassword.scss';
+import '@styles/RecoveryPassword.scss';
+import logo_yard_sale from '@logos/logo_yard_sale.svg';
+import icon_email from '@icons/email.svg';
 
 const RecoveryPassword = () => {
   return (
-    <div className="login">
-      <div className="form-container">
-        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+    <div className="recovery-password-container">
+      <div className="recovery-password-form-container">
+        <img src={logo_yard_sale} alt="logo" className="logo" />
         <h1 className="title">Email has been sent!</h1>
         <p className="subtitle">Please check your inbox for instructions on how to reset the password</p>
         <div className="email-image">
-          <img src="./icons/email.svg" alt="email" />
+          <img src={icon_email} alt="email" />
         </div>
         <button className="primary-button login-button">Login</button>
         <p className="resend">
