476f442763bb9fbcb75cb2a35d4d9dc6acf338b8
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-02-01 23:28:58 -0500

    Donde estaba la palabra login en los className, se reemplazó por new-password.

--- src/containers/NewPassword.jsx
+++ src/containers/NewPassword.jsx
@@ -4,17 +4,17 @@ import logo_yard_sale from '@logos/logo_yard_sale.svg';
 
 const Login = () => {
   return (
-    <div className="login-container">
-      <div className="login-form-container">
+    <div className="new-password-container">
+      <div className="new-password-form-container">
         <img src={logo_yard_sale} alt="logo" className="logo" />
         <h1 className="title">Create a new password</h1>
         <p className="subtitle">Enter a new passwrd for yue account</p>
-        <form action="/" className="login-form">
+        <form action="/" className="new-password-form">
           <label htmlFor="password" className="label">Password</label>
           <input type="password" id="password" placeholder="*********" className="input input-password" />
           <label htmlFor="new-password" className="label">Password</label>
           <input type="password" id="new-password" placeholder="*********" className="input input-password" />
-          <input type="submit" value="Confirm" className="primary-button login-button" />
+          <input type="submit" value="Confirm" className="primary-button confirm-button" />
         </form>
       </div>
     </div>
