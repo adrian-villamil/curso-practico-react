1e3bf143464a6a07b7b339c3d5324df3fb681559
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-02-05 17:35:55 -0500

    Agregamos el menu para mobile.

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import '@styles/MobileMenu.scss';
+
+const MobileMenu = () => {
+  return (
+    <div class="mobile-menu">
+      <div>
+        <ul>
+          <li><a href="/">CATEGORIES</a></li>
+          <li><a href="/">All</a></li>
+          <li><a href="/">Clothes</a></li>
+          <li><a href="/">Electronics</a></li>
+          <li><a href="/">Furnitures</a></li>
+          <li><a href="/">Toys</a></li>
+          <li><a href="/">Others</a></li>
+        </ul>
+        <ul>
+          <li><a href="/">My orders</a></li>
+          <li><a href="/">My account</a></li>
+        </ul>
+      </div>
+      <ul>
+        <li><a href="/" class="email">platzi@example.com</a></li>
+        <li><a href="/" class="sign-out">Sign out</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default MobileMenu;
