72fc0460fe359d75fca1ebf30aa8e3b1e34deecd
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-02-05 21:47:22 -0500

    Le agregamos un estado.

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import '@styles/ProductItem.scss';
 import bt_add_to_cart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+  const [cart, setCart] = useState('Hola');
+
+  const handleClick = () => {
+    setCart('Hola Mundo');
+  };
+
   return (
     <div className="product-item">
       <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="product-item" />
@@ -11,9 +17,10 @@ const ProductItem = () => {
           <p>$120,00</p>
           <p>Bike</p>
         </div>
-        <figure>
+        <figure onClick={handleClick}>
           <img src={bt_add_to_cart} alt="cart" />
         </figure>
+        {cart}
       </div>
     </div>
   );
