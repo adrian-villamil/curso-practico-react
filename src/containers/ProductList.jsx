75f17a94f0c989e035676bf07ad33c2e5b35adbe
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-02-06 15:55:05 -0500

    Hicimos fetch a una API de platzi utilizando axios.

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '@styles/ProductList.scss';
 import ProductItem from "../components/ProductItem";
+import axios from "axios";
+
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await axios.get(API);
+        setProducts(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getProducts();
+  }, []);
+
   return (
     <section className="main-container">
       <div className="product-list">
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
+        {products.map(product => (
+          <ProductItem key={product.id} />
+        ))}
       </div>
     </section>
   );
