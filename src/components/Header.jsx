27fe70ab4ab577aa9045117654ba6d42927b0db2
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-02-06 15:31:02 -0500

    Se le agregó un estado y un modificador para poder hacer toggle al DesktopMenu.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/Header.scss';
 import icon_menu from '@icons/icon_menu.svg';
 import logo_yard_sale from '@logos/logo_yard_sale.svg';
 import icon_shopping_cart from '@icons/icon_shopping_cart.svg';
+import DesktopMenu from '@components/DesktopMenu';
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <nav>
       <img src={icon_menu} alt="menu" className="header-menu-icon" />
@@ -33,13 +40,16 @@ const Header = () => {
       </div>
       <div className="navbar-right">
         <ul>
-          <li className="navbar-email">platzi@example.com</li>
+          <li className="navbar-email" onClick={handleToggle}>
+            platzi@example.com
+          </li>
           <li className="navbar-shopping-cart">
             <img src={icon_shopping_cart} alt="shopping cart" />
             <div>2</div>
           </li>
         </ul>
       </div>
+      {toggle && <DesktopMenu />}
     </nav>
   );
 }
