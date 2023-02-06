ecda8ae0f977622b6cc20f3e4dec99a47307ecb5
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-02-05 20:16:07 -0500

    Se agregó nuevas rutas.

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import NewPassword from '../containers/NewPassword';
+import NewPassword from '../pages/NewPassword';
 import RecoveryPassword from '../containers/RecoveryPassword';
+import CreateAccount from '../pages/CreateAccount';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import MyAccount from '../pages/MyAccount';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
+import Orders from '../pages/Orders';
+import OrderDetail from '../pages/OrderDetail';
 
 const App = () => {
   return (
@@ -13,6 +18,11 @@ const App = () => {
       <Layout>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/create-account' element={<CreateAccount />} />
+          <Route path='/my-account' element={<MyAccount />} />
+          <Route path='/my-orders' element={<Orders />} />
+          <Route path='/order' element={<OrderDetail />} />
           <Route path='/new-password' element={<NewPassword />} />
           <Route path='/recovery-password' element={<RecoveryPassword />} />
           <Route path='*' element={<NotFound />} />
