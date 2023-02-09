6131e9160c825c8ac5e6bde53b6ec02fdb1b4d30
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-02-09 00:54:04 -0500

    Deplo final, no me funciona las rutas ni con browserrouter, ni con hashrouter.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'curso-practico-react/bundle.js',
     publicPath: '/curso-practico-react/'
   },
   mode: 'development',
